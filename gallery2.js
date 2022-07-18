cec6df6ee293b835082dfe21c33715d37faf296f
adichiara <adamdichiara@gmail.com>
2022-07-18 08:25:28 -0400

    update

--- gallery2.js
+++ gallery2.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function () {
 
-	var source_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSzskwMAka_TYxIV33UEBLOrE2b46sPRd7oiKaxx5KC_kSUU_lKJpIkgPVB-iHV5LTH3LaDE-lwUQgm/pub?gid=150341003&single=true&output=csv';
-	
+	var source_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS_0YBYvDYcxDoHXPuRjYU9NZe_e-ezOtIjYoVaepDGhWE0waffJT59tVxI0F_JGytHWB5s52DnxnFq/pub?gid=372625441&single=true&output=csv';
+
 	d3.csv(source_url, function(d) {
 //	 	console.log(d[0]);
 		d.id = +d.id;
@@ -38,9 +38,15 @@ $(document).ready(function () {
 		
 		var modal = document.getElementById("myModal");
 		var modalImg = document.getElementById("modal_img");
+		var modalSelected = document.getElementById("modal_selected");
+		var closeButton = document.getElementById("close");
+		var resetButton = document.getElementById("reset");
+		var selectionsButton = document.getElementById("show_selections_btn");
 		var captionText = document.getElementById("caption");
+
 		var path;
 		var selected_images = [];
+		var confirmed_images = [];
 				
 		
 		//populate archive dropdown
@@ -68,11 +74,7 @@ $(document).ready(function () {
 		});
 		
 		
-		function copyLink(x) {
-			document.execCommand('copy');
-			console.log(x);
-		}
-
+		
 		function showImages() {
 		
 			$('#gallery').empty()
@@ -90,13 +92,13 @@ $(document).ready(function () {
 				img.id = id_name;
 				img.src = image_url_thumb;  
 				img.title = image_url_full;
-				img.classList.add('img-responsive','image');
+				img.classList.add('img-responsive','thumb');
 				$('#gallery').append(img);
 			}
 			
 
 
-			$(document).on('click','.image', function() {
+			$(document).on('click','.thumb', function() {
 					id_name = $(this).attr('id');	
 					console.log(id_name);
 
@@ -111,28 +113,81 @@ $(document).ready(function () {
 					// select
 					else {  
 						$(this).addClass('selected');		
+						modalSelected.style.display = 'none';
+						modalImg.style.display = 'block';	
 						modal.style.display = "block";
 						modalImg.src = url;
 						captionText.innerHTML = url;
-						copyLink(url);
-						//selected_images.push(url);
+						selected_images.push(url);
 					}
-
-				$('#output_area').empty();			
-					for (var i=0; i<selected_images.length; i++) {
-						var output = document.createElement("p");
-						output.innerHTML = selected_images[i];
-
-						$('#output_area').append(output);			
-					}
-
 				});
+
+
+			$(document).on('click','.modal-selected', function() {
+					id_name = $(this).attr('id');	
+					console.log(id_name);
+
+					url = $(this).attr('title');				
+					console.log(url);
+
+					// unselect
+					if ($(this).hasClass('confirmed')) {
+						$(this).removeClass('confirmed');
+						confirmed_images = confirmed_images.filter(item => item !== url)	
+					} 
+					// select
+					else {  
+						$(this).addClass('confirmed');		
+						modalSelected.style.display = 'none';
+						modalImg.style.display = 'block';	
+						modal.style.display = "block";
+						modalImg.src = url;
+						captionText.innerHTML = url;
+						selected_images.push(url);
+					}
+				});
+
+
+				// $('#output_area').empty();			
+				// 	for (var i=0; i<selected_images.length; i++) {
+				// 		var output = document.createElement("p");
+				// 		output.innerHTML = selected_images[i];
+				// 		// $('#output_area').append(output);			
+				// 	}
 
 
 			modalImg.onclick = function(){
 				modal.style.display = "none";
+				resetButton.style.display = 'none';
 			}
-		
+			closeButton.onclick = function(){
+				modal.style.display = "none";
+				resetButton.style.display = 'none';
+			}
+			
+			selectionsButton.onclick = function(){
+				$('#modal_selected').empty();
+				modalImg.style.display = 'none';	
+				resetButton.style.display = 'block';
+				modalSelected.style.display = 'block';
+					
+				captionText.style.display = 'none';
+
+				console.log(selected_images);
+				modal.style.display = "block";
+				
+				for (var i=0; i<selected_images.length; i++) {	
+					
+					var img = document.createElement('img');
+					/* img.id = id_name; */
+					img.src = selected_images[i];  
+					img.classList.add('img-responsive','modal-selected');
+					$('#modal_selected').append(img);		
+				}
+				
+				/* modal.style.display = "none" */
+			}
+			
 		}
 
 
